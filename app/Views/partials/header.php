2b7ad4431b438959146903a1bfb70df48bc5da82
Marco Ramirez <marcorape07@gmail.com>
2019-12-03 02:35:28 -0600

    Add css to header

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,7 +1,10 @@
 <head>
     <meta charset="utf-8">
-    <title>MyApp</title>
+    <title>AvanSoftware</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="/assets/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed" rel="stylesheet">
 </head>
 <body>
