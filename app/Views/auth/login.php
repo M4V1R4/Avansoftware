66739075ec5a8b7349ab4106c34c030bf806e0db
Marco Ramirez <marcorape07@gmail.com>
2019-12-03 02:33:28 -0600

    Edit paths

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,33 +1,19 @@
 <?php include VIEWS.'/partials/header.php' ?>
+<div class="banner">
 <?php include VIEWS.'/partials/navbar.php' ?>
-  <div class="container">
+  
     <div class="row">
-      <div class="col-md-12">
-        <?php include VIEWS.'/partials/message.php' ?>
-      </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <h1>Iniciar sesión</h1>
-          <!-- Inicia el formulario de Login -->
-          <form action="/authenticate/index.php?action=auth" method="post">
-            <div class="form-group">
-              <label for="username">Usuario:</label>
-              <input
-                type="text" class="form-control"
-                id="username" name="username"
-                aria-describedby="Introduzca su nombre de usuario"
-                placeholder="">
-            </div>
-            <div class="form-group">
-              <label for="password">contraseña:</label>
-              <input type="password" class="form-control"
-                id="password" name="password"
-                placeholder="">
-            </div>
-            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+        <div class="col-md-12">
+        <div class="wrapper">
+          <form action="/authenticate/index.php?action=auth" class="form-signin" method="POST">
+              <h2 class="form-signin-title text-center">Inicio de Sesión</h2>
+              <input type="text" class="form-control" name="username" placeholder="Usuario" autofocus=""/>
+              <input type="password" class="form-control" name="password" placeholder="Contraseña" />
+              <button class="btn btn-lg btn-success btn-block" type="submit">Iniciar Sesión</button>
+              <label for="">¿No tienes una cuenta?<a href="/users/index.php?action=register"> Registrate aquí.</a></label>
           </form>
         </div>
+        </div>
     </div>
-  </div>
+  
   <?php include VIEWS.'/partials/footer.php' ?>
