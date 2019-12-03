d14aaf7143470b2003844d362db0516a9d3d05bf
Marco Ramirez <marcorape07@gmail.com>
2019-12-03 02:34:21 -0600

    Add div close

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,3 +1,4 @@
+</div>
     <footer>
         <script src="/assets/js/jquery-3.3.1.min.js" charset="utf-8"></script>
         <script src="/assets/js/bootstrap.bundle.min.js" charset="utf-8"></script>
