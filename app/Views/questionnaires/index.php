b95084099c5b190db7f359fc6a7ff339f0cc37d2
Marco Ramirez <marcorape07@gmail.com>
2019-12-04 03:01:42 -0600

    Add questionnaires view

---
+++ app/Views/questionnaires/index.php
@@ -0,0 +1,32 @@
+<?php include VIEWS.'/partials/header.php' ?>
+<?php include VIEWS.'/partials/navbar.php' ?>
+  
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <h1 class="text-center">Índice de Cuestionarios</h1>
+            <hr>
+            <table class="table table-hover">
+            <thead class="thead-dark">
+                <tr class="text-center">
+                <th scope="col">#</th>
+                <th scope="col">Cuestionario</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($collection as $record) : ?>
+                <tr class="text-center">
+                    <th scope="row"><?= $record["id"] ?></th>
+                    <td><?= $record["description"] ?></td>
+                    <td class="text-center">
+                        <a type="button" class="btn btn-primary" href="/users/index.php?action=see&id=<?=$record["id"]?>" >Responder</a>
+                    </td>                    
+                </tr>
+                <?php endforeach; ?>
+             </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
+  <?php include VIEWS.'/partials/footer.php' ?>
