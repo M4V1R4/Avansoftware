938e007931676159286316a9619db5f32f320c03
Marco Ramirez <marcorape07@gmail.com>
2019-12-09 03:19:11 -0600

    Add view see

---
+++ app/Views/questionnaires/see.php
@@ -0,0 +1,39 @@
+<?php
+    $id = isset($id) ? $id : $questionnaire['id'];
+    $description = isset($description) ? $description : $questionnaire['description'];
+    $long_description = isset($long_description) ? $long_description : $questionnaire['long_description'];
+?>
+<?php include VIEWS.'/partials/header.php' ?>
+<?php include VIEWS.'/partials/navbar.php' ?>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <?php include VIEWS.'/partials/message.php' ?>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+        <br>
+          <h1 class="text-center">Ver Cuestionario</h1>
+          <hr>
+          <!-- Inicia el formulario de ver -->
+          
+            <div class="form-group">
+              <label for="description">Descripción:</label>
+              <input
+                type="text" class="form-control"
+                id="description" name="description"
+                placeholder="" value="<?= isset($description) ? $description : ""; ?>"  disabled>
+            </div>
+            <div class="form-group">
+              <label for="long_description">Descripción larga:</label>
+              <textarea class="form-control"
+                id="long_description" name="long_description" rows="5" cols="50" style="resize: none;" disabled><?= isset($long_description) ? $long_description : ""; ?></textarea>
+            </div>
+          <a type="button" class="btn btn-primary">Ver Reporte</a>
+          <a href="/questionnaires/index.php?action=edit&id=<?= $id ?>" type="button" class="btn btn-primary">Editar</a>
+          <a href="/questionnaires/index.php?action=maintenance" type="button" class="btn btn-primary">Regresar a la lista</a>
+        </div>
+    </div>
+  </div>
+  <?php include VIEWS.'/partials/footer.php' ?>
