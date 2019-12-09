86a7a3c07a4ce14e450a8e81095fecb0d125e78a
Marco Ramirez <marcorape07@gmail.com>
2019-12-09 03:20:38 -0600

    Add methods to questionnaire controller

--- app/Controllers/QuestionnaireController.php
+++ app/Controllers/QuestionnaireController.php
@@ -15,15 +15,80 @@ class QuestionnaireController
         $this->message = new Message();
     }
 
-    public function index()
-    {
+    public function index(){
+        $this->questionnaireModel = new Questionnaire($this->config);
+        $id = $_SESSION['login']['id'];
+        $message = $this->message;
+        $collection = $this->questionnaireModel->getAllQuestionnairesByUser($id);
+        view("questionnaires/index.php", compact("collection", "message"));
+    }
+    public function reply(){
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $this->questionnaireModel = new Questionnaire($this->config);
+        $questionnaire = $this->questionnaireModel->getQuestionnaire($id);
+        
+        view("questionnaires/questionnaire.php", compact("questionnaire"));
+    }
+    public function maintenance(){
         $this->questionnaireModel = new Questionnaire($this->config);
         $message = $this->message;
         $collection = $this->questionnaireModel->getAllQuestionnaires();
-        
-        //var_dump($colletion);
-        view("questionnaires/index.php", compact("collection", "message"));
+        view("questionnaires/m_questionnaires.php", compact("collection", "message"));
     }
+    public function see(){
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $this->questionnaireModel = new Questionnaire($this->config);
+        $questionnaire = $this->questionnaireModel->getQuestionnaire($id);
+        
+        view("questionnaires/see.php", compact("questionnaire"));
+    }
+    public function edit(){
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $this->questionnaireModel = new Questionnaire($this->config);
+        $questionnaire = $this->questionnaireModel->getQuestionnaire($id);
+        view("questionnaires/edit.php", compact("questionnaire"));
+    }
+    public function destroy(){
+        $id = isset($_GET['id']) ? $_GET['id'] : null;
+        $this->questionnaireModel = new Questionnaire($this->config);
 
+        if ($id != null)
+        {
+            // Si pasó todas la verificaciones hago el delete
+            $this->questionnaireModel->delete($id);
+            $this->message->setSuccessMessage(null, "Se eliminó el registro correctamente", null, true);
+            $this->maintenance();
+            // view("/questionnaires/index.php?action=maintenance");
+        }
+        else
+        {
+            $this->message->setWarningMessage(null, "No sé cuál registro borrar", null, true);
+            $this->maintenance();
+            // view("/questionnaires/index.php?action=maintenance");
+        }
+    }
+    public function create(){
+        view("questionnaires/create.php");
+    }
+    public function store(){
+        $message = new Message();
+        $description         = $_POST["description"];
+        $long_description         = $_POST["long_description"];
+
+        $questionnaireModel = new Questionnaire($this->config);
+        
+
+        // Verificar que todos los inputs estén llenos
+        if ((ltrim($description) == "") || (ltrim($long_description) == ""))
+        {
+            $message->setWarningMessage(null, "Todos los campos son requeridos", null, true);
+            view("questionnaires/create.php", compact("message", "description", "long_description"));
+            exit;
+        }
+        // Si pasó todas la verificaciones hago el insert
+        $questionnaireModel->insert($description, $long_description);
+        $this->message->setSuccessMessage(null, "El registro se agregó correctamente", null, true);
+        $this->maintenance();
+    }
 }
 }
